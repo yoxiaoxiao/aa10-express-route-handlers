871d1c8a9802584f1ddc7f1ea93e8c0337fe5b82
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-10-19 16:41:19 -0700

    finished first 2

--- server/app.js
+++ server/app.js
@@ -25,6 +25,24 @@ const express = require('express');
 const app = express();
 
 // Your code here 
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+// get all the artists
+app.get("/artists", (req, res) => {
+  const artists = getAllArtists();
+  res.status(200).json(artists);
+});
+
+// add an artist
+app.post("/artists", (req, res) => {
+  const artistData = req.body;
+  const newArtist = addArtist(artistData);
+  res.status(201).json(newArtist);
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
